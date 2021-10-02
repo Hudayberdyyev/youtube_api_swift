70044dd834c60067aa7528ad5a3edc3cfe0495a5
Hudayberdyyev <hudayberdyyev.ahmet@gmail.com>
2021-10-03 01:36:49 +0500

    JSON parsing 2

--- youtube_api_swift/Video.swift
+++ youtube_api_swift/Video.swift
@@ -7,11 +7,53 @@
 
 import Foundation
 
-struct Video {
+struct Video : Decodable {
     var videoId = ""
     var title = ""
     var description = ""
     var thumbnail = ""
-    var published = ""
+    var published = Date()
+    
+    // coding keys which must parsed from JSON response
+    enum CodingKeys: String, CodingKey {
+        case snippet
+        case thumbnails
+        case high
+        case resourceId
+        
+        case published = "publishedAt"
+        case title
+        case description
+        case thumbnail = "url"
+        case videoId
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let snippetContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .snippet)
+        
+        // Parse title
+        self.title = try snippetContainer.decode(String.self, forKey: .title)
+        
+        // Parse description
+        self.description = try snippetContainer.decode(String.self, forKey: .description)
+        
+        // Parse publishdate
+        self.published = try snippetContainer.decode(Date.self, forKey: .published)
+        
+        // Parse thumbnails
+        let thumbnailsContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .thumbnails)
+        
+        let highContainer = try thumbnailsContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .high)
+        
+        self.thumbnail = try highContainer.decode(String.self, forKey: .thumbnail)
+        
+        // Parse videoId
+        
+        let resourceContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .resourceId)
+        
+        self.videoId = try resourceContainer.decode(String.self, forKey: .videoId)
+    }
 }
  
